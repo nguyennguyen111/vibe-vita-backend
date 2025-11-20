340894fbe8f4aa8ea3e4dd9e306b0e5308273b00
nguyennguyen111 <nguyenhnde170514@gmail.com>
2025-11-20 21:40:47 +0700

    Up

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
-const multer = require('multer')
+const multer = require('multer') //////////////
 const path = require('path')
 const fs = require('fs')
 
